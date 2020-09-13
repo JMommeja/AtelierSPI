b2c75b73342e3bfacf06243eb20fde2e454634e1
Jean-François Giammari <36701005+JF-GIAMMARI@users.noreply.github.com>
2020-09-13 11:57:44 +0200

    Update ex3.py
    Little changes and Little optimisations (Waiting for more exercices)

--- Atelier2/ex3.py
+++ Atelier2/ex3.py
@@ -1,21 +1,21 @@
 def descriminant ( a:float,b:float,c:float)-> float:
-    """ calcul delta """
-    return  b**2-4*a*c
+    """ Calcul delta """
+    return b*b - (4*a*c)
    
 
 def racine_unique(a:float,b:float) -> float:
-    """calcul racine unique """
-    return  -b/2*a
+    """ Calcul racine unique """
+    return (-1*b)/(2*a)
     
 def racine_double(a:float,b:float,delta:float,num:float)->float:
-    """renvoie la valeur  de la racine"""
+    """ Renvoie la valeur  de la racine"""
     if num == 1:
         return  -b + delta**(0.5)/2*a
     else:
         return -b - delta**(0.5)/2*a
 
 def str_equation(a,b,c)->str:
-    """renvoie chaine caractere equation"""
+    """ Renvoie chaine caractere equation"""
     VALEUR_EGAL = 0
     equation = ""
     
@@ -46,13 +46,13 @@ def str_equation(a,b,c)->str:
     return equation
 
 def solution_equation(a:float, b:float, c:float):
-    """renvoie chaine caractere redige"""
-
-    if descriminant(a,b,c)<0:
+    """ Renvoie chaine caractere redige"""
+    delta = descriminant(a,b,c)
+    if delta<0:
         return "Solution equation "+str_equation(a,b,c)+" Pas de racine"
-    if descriminant(a,b,c)==0:
+    if delta==0:
         return "Solution equation ",a,"x2 + ",b,"x + ",c," = 0 une racine unique x="+racine_unique(a,b)
-    if descriminant(a,b,c)>0:
+    if delta>0:
         return "Solution equation ",a,"x2 + ",b,"x + ",c," = 0 deux racines""x1 =" 
 
     
